ecad1227ad24d605c560ea0f690736eb4778870f
Jerome <jerome.thai.87@gmail.com>
2014-09-05 18:34:15 -0700

    implement optimal pricing

---
+++ optimal_pricing.py
@@ -0,0 +1,63 @@
+'''
+Created on Sep 5, 2014
+
+@author: jeromethai
+'''
+
+from cvxopt import matrix
+from utility_estimator import x_solver
+
+"""
+def x_solver(Q, r, p, H, z, soft):
+    Optimization w.r.t. x-block
+    
+    Parameters
+    ----------
+    Q, r: parameters of the quadratic utility function
+    p: price vector
+    H: observation matrix
+    z: observed demand vector
+    soft: weight on the observation
+
+def mis_solver(zs, H, ps, smooth, soft=1e6, max_iter=3):
+    Solves the inverse optimization problem with missing values
+    
+    Parameters
+    ----------
+    zs: list of observed demand vectors
+    H: observation matrix
+    ps: list of price vectors
+    smooth: regularization parameter on r
+    soft: weight on the observation
+    max_iter: number of iterations
+    
+    n = H.size[1]
+    Q, r = -spdiag([10.]*n), matrix(10., (n,1))
+    for k in range(max_iter):
+        xs = [x_solver(Q, r, p, H, z, soft) for p,z in zip(ps,zs)]
+        Q, r = inv_solver(xs, ps, compute_xmax(xs), smooth)
+    return Q, r
+"""
+
+def solver(Q, r, i, d, p, soft=1e6, max_iter=3):
+    """Pricing for product i that matches desired demand d
+    given other prices p and quadratic utility function (Q,r)
+    
+    Parameters
+    ----------
+    (Q,r): quadratic utility function
+    i: computes optimal price for product i, 1<=i<=n
+    d: target demand for product i
+    p: full price vector (price of product i, p^i, is arbitrary) 
+    max_iter: number of iterations
+    """
+    n = len(r)
+    H = matrix(0., (1,n)); H[i-1] = 1.
+    for k in range(max_iter):
+        x = x_solver(Q, r, p, H, d, soft)
+        p[i-1] = max((H*(Q*x+r))[0], 0)
+    return p
+
+
+if __name__ == '__main__':
+    pass
--- utility_estimator_test.py
+++ utility_estimator_test.py
@@ -5,29 +5,71 @@ Created on Sep 3, 2014
 '''
 
 
+import numpy as np
 from numpy import linalg as la
 from cvxopt import matrix
 import utility_estimator as est
 import generate_samples as sample
 import Utility as U
+import matplotlib.pyplot as plt
+
+
+def display_results(u1, u2, u_true, error1, error2, xmax):
+    """Display results
+    
+    Parameters
+    ----------
+    u1: estimated utility function 1
+    u2: estimated utility function 2
+    u_true: true utility function
+    error1: relative error in estimated demand from u1
+    error2: relative error in estimated demand from u2
+    xmax: maximum prices vector
+    """
+    index = [0,2]
+    for i in index:
+        xdata = np.linspace(0.0, xmax[i], num=100)
+        e = matrix([0.]*5); e[i] = 1.0
+        vals1 = [u1.utility(matrix(x*e)) for x in xdata]
+        vals2 = [u2.utility(matrix(x*e)) for x in xdata]
+        true_vals = [u_true.utility(matrix(x*e))[0] for x in xdata]
+        offset1 = np.mean(true_vals) - np.mean(vals1)
+        offset2 = np.mean(true_vals) - np.mean(vals2)
+        plt.plot( xdata, [offset1+v for v in vals1], 'b', label='full')
+        plt.plot( xdata, [offset2+v for v in vals2], 'g', label='missing')
+        plt.plot( xdata, true_vals, 'r', label='true')
+        plt.xlabel('Demand in product {}'.format(i+1))
+        plt.ylabel('Utility')
+        plt.title('Estimated utility function, err1={:.0f}%, err2={:.0f}%'.format(100*error1, 100*error2))
+        plt.legend(loc=0)
+        plt.show()
 
 
 def test(N, model, K):
-    """Test utility_estimator"""
+    """Test utility_estimator
+    
+    Parameters:
+    -----------
+    N: number of samples
+    model: utility function model
+    K: missing value is demand in product K
+    """
     u, data = sample.sample_utility_data(N, model)
     data_mis, H = est.remove_values(data, K)
     Q1, r1, smooth1 = est.main_solver(data)
     Q2, r2, smooth2 = est.main_solver(data_mis, H)
     u1 = U.Utility((Q1,r1), 'quad')
     u2 = U.Utility((Q2,r2), 'quad')
-    x_true = matrix([d[0] for d in data])
+    xs = [d[0] for d in data]
+    x_true = matrix(xs)
     x1 = matrix([u1.compute_demand(d[1]) for d in data])
     x2 = matrix([u2.compute_demand(d[1]) for d in data])
-    print smooth1, smooth2
-    print la.norm(x1-x_true, 1)/la.norm(x_true, 1)
-    print la.norm(x2-x_true, 1)/la.norm(x_true, 1)
-    print H
-            
+    e1 = la.norm(x1-x_true, 1)/la.norm(x_true, 1)
+    e2 = la.norm(x2-x_true, 1)/la.norm(x_true, 1)
+    print e1,e2
+    xmax = est.compute_xmax(xs)
+    display_results(u1, u2, u, e1, e2, xmax)            
+
 
 def main():
     test(200, 2, 0)
